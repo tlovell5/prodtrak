2298a5779bc4cc9d53da148bad2e4c2696c8e4b5
Your Name <you@example.com>
2023-08-25 21:17:26 -0600

    Logo now working too with some css grid functionality

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,12 @@
 import React from "react";
+import logo from "./prodtraklogo.png"; // Importing the logo
 
 function Sidebar({ setCurrentPage }) {
   return (
     <div className="sidebar">
+      <div className="logo-container">
+        <img src={logo} alt="ProdTrak Logo" className="sidebar-logo" />
+      </div>
       <ul>
         <li onClick={() => setCurrentPage("Data Entry")}>Data Entry</li>
         <li onClick={() => setCurrentPage("Data Tables")}>Data Tables</li>
